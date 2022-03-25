f22da4180b59f1accd8c93aea77d29fbbb728231
Petar Zhelev <kain@matrix-main.local>
2022-03-25 15:45:41 +0200

    exercise

--- Fundamentals/Arrays Exercise/maxSequenceOfEqualElements.js
+++ Fundamentals/Arrays Exercise/maxSequenceOfEqualElements.js
@@ -1,4 +1,9 @@
 function solve(arr) {
+  /*   Write a function that determines if there exists an element in the array of numbers 
+  such that the sum of the elements on its left is equal to the sum of the elements on its right. 
+  If there are NO elements to the left/right, their sum is 0. 
+  Print the index that satisfies the required condition or "no" if there is no such index. */
+
   let newArr = [];
   let tempArr = [];
 
