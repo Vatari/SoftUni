628194b220304f56707f6123e0d7d714ba9404fb
unknown <majestic13@gmail.com>
2022-03-27 21:06:15 +0300

    exercise

--- solve.js
+++ solve.js
@@ -1,38 +1,37 @@
 function solve(arr) {
   let encryptedText = arr.shift();
+  let text;
 
   while (arr[0] != "Decode") {
     let tokens = arr.shift().split("|");
-
     command = tokens[0];
 
     if (command === "Move") {
-      Move(encryptedText, tokens[1]);
-    } else if (command === "Insert") {
-      Insert(encryptedText, tokens[1], tokens[2]);
+      encryptedText = Move(encryptedText, tokens[1]);
+
+     } else if (command === "Insert") {
+      encryptedText = Insert(encryptedText, tokens[1], tokens[2]);
     } else if (command === "ChangeAll") {
-      ChangeAll(encryptedText, tokens[1], tokens[2]);
+      encryptedText = ChangeAll(encryptedText, tokens[1], tokens[2]);
     }
-}
+  }
 
-    function Move(text, num) {
-      let half = text.substring(0, num);
-      let right = text.substring(num);
-        newText = right + half;
-      return newText;
-    }
-    function Insert(text, num, value) {
-      let half = text.substring(0, num);
-      let right = text.substring(num);
-      newText = half + value + right;
-      return newText;
-    }
-    function ChangeAll(text, str, replacement) {
-      newText = text.split(str).join(replacement);
-      return newText;
-    }
-  
+  function Move(text, num) {
+    let half = text.substring(0, num);
+    let right = text.substring(num);
+    text = right + half;
+    return text;
+  }
+  function Insert(text, num, value) {
+    let half = text.substring(0, num);
+    let right = text.substring(num);
+    text = half + value + right;
+    return text;
+  }
+  function ChangeAll(text, str, replacement) {
+    text = text.split(str).join(replacement);
+    return text;
+  }
   console.log(`The decrypted message is: ${encryptedText}`);
-
 }
 solve(["zzHe", "ChangeAll|z|l", "Insert|2|o", "Move|3", "Decode"]);
