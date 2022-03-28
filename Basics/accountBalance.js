f2c006420ddc4cbf8d7c11219eadf842d802dc30
unknown <majestic13@gmail.com>
2022-03-28 17:12:21 +0300

    exercise

---
+++ Basics/accountBalance.js
@@ -0,0 +1,20 @@
+function accountBalance(input) {
+  let accountBalance = 0;
+  let index = 0;
+  let command = input[index];
+  index++;
+
+  while (command !== "NoMoreMoney") {
+    let money = Number(command);
+    if (money < 0) {
+      console.log("Invalid operation!");
+      break;
+    }
+    accountBalance += money;
+    console.log(`Increase: ${money.toFixed(2)}`);
+    command = input[index];
+    index++;
+  }
+  console.log("Total: " + accountBalance.toFixed(2));
+}
+accountBalance(["120", "45.55", "-150"]);
