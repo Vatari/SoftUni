e120fb20bdda1a23089885fe280c039ca1ac8a61
unknown <majestic13@gmail.com>
2022-03-28 17:55:23 +0300

    exercise

---
+++ Basics/ageAndSex.js
@@ -0,0 +1,24 @@
+function ageAndSex (input) {
+
+    let age = Number(input[0]);
+    let gender = input [1];
+
+    if (age >= 16) {
+        if (gender === "m") {
+            console.log ("Mr.");
+        } else {
+            console.log("Ms.");
+        }
+
+    } else {
+        if (gender === "m") {
+            console.log("Master");
+        }else {
+            console.log ("Miss");
+        }
+
+
+    }
+
+}
+ageAndSex (["25", "f"]);
