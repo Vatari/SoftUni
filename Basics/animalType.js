9897beb5a3f2e7d60da69611977141b1f00a4fa4
unknown <majestic13@gmail.com>
2022-03-29 08:05:07 +0300

    exercise

---
+++ Basics/animalType.js
@@ -0,0 +1,16 @@
+function animalType(input) {
+  let typeAnimal = input[0];
+
+  if (typeAnimal === "dog") {
+    console.log("mammal");
+  } else if (
+    typeAnimal === "snake" ||
+    typeAnimal === "tortoise" ||
+    typeAnimal === "crocodile"
+  ) {
+    console.log("reptile");
+  } else {
+    console.log("unknown");
+  }
+}
+animalType(["snake"]);
