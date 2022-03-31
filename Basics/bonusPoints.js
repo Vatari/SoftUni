f8dae1c2892a8c17a791ab407e5dda2d1b971fd8
Petar Zhelev <majestic13@gmail.com>
2022-03-31 14:56:44 +0300

    exercise

---
+++ Basics/bonusPoints.js
@@ -0,0 +1,21 @@
+function bonusPoints(input) {
+  let score = Number(input[0]);
+  let bonus = 0;
+
+  if (score <= 100) {
+    bonus = 5;
+  } else if (score < 1000) {
+    bonus = score * 0.2;
+  } else if (score > 1000) {
+    bonus = score * 0.1;
+  }
+
+  if (score % 2 === 0) {
+    bonus = bonus + 1;
+  } else if (score % 5 === 0) {
+    bonus = bonus + 2;
+  }
+
+  console.log(`${bonus} ${score + bonus}`);
+}
+bonusPoints(["20"]);
