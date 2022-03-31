d5532b3de449d1ded5c17a178942eb033dc3d279
Petar Zhelev <majestic13@gmail.com>
2022-03-31 14:15:00 +0300

    exercise

---
+++ Basics/basketball.js
@@ -0,0 +1,10 @@
+function basketballExpenses(input) {
+	let tax = Number(input[0]);
+	let shoes = tax * 0.6;
+	let equip = shoes * 0.8;
+	let ball = equip * (1 / 4);
+	let accesories = ball * (1 / 5);
+	let totalSum = tax + shoes + equip + ball + accesories;
+	console.log(totalSum);
+}
+basketballExpenses(['365']);
