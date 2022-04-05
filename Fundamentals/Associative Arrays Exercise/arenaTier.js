33c5d5bae8095e0795db768a20515c9955a81cde
unknown <majestic13@gmail.com>
2022-04-05 11:51:22 +0300

    exercise

--- Fundamentals/Associative Arrays Exercise/arenaTier.js
+++ Fundamentals/Associative Arrays Exercise/arenaTier.js
@@ -5,44 +5,29 @@ function solve(arr) {
     let [name, skill, skillPoints] = arr.shift().split(" -> ");
     skillPoints = +skillPoints;
 
-    
-    
-    if (!gladiators[name]) {
-
-      gladiators[name] = [skill, skillPoints]
-      console.log(gladiators[name][skill]);
-      if (gladiators[name][skill] < skillPoints) {
-        console.log('sdsdasd');
-           gladiators[name].push(skill, skillPoints)
-           gladiators[name][skill] = skillPoints
-         }
-         } 
-         
-         
-         
-         
-
-
-    
-    
-   /*  if (gladiators[name][0].includes(skill)) {
-        if (gladiators[name][1] < skillPoints) {
-            gladiators[name][1] = skillPoints;
-            
+    if (!gladiators.hasOwnProperty(name)) {
+      gladiators[name] = [skill, skillPoints];
+    } else {
+      if (!gladiators[name].includes(skill)) {
+        gladiators[name].push(skill, skillPoints);
+      } else {
+        let oldPoints = gladiators[name][1];
+        if (oldPoints < skillPoints) {
+          gladiators[name][0][1] = skillPoints;
         }
-    } */
-
-
-    //  if (gladiators[name].)
+      }
+    }
   }
-console.log(gladiators);
+  console.log(gladiators);
 }
 solve([
   "Peter -> BattleCry -> 400",
   "Alex -> PowerPunch -> 300",
+  "Stefan -> Duck -> 12",
   "Stefan -> Duck -> 2223423423400",
+  "Stefan -> Duck -> 2",
   "Stefan -> Tiger -> 11250",
+  "Stefan -> Tiger -> 1250",
   "Stefan -> Tiger -> 1111250",
-
   "Ave Cesar",
 ]);
