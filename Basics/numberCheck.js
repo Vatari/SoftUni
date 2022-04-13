e90a1aaa5e8de82bdf0fe62a32cac7603c345621
Petar Zhelev <majestic13@gmail.com>
2022-04-13 21:42:46 +0300

    exercise

---
+++ Basics/numberCheck.js
@@ -0,0 +1,14 @@
+function numberCheck(input) {
+
+    let number = Number(input[0]);
+
+    if (number < 100) {
+        console.log("Less than 100");
+    } else if (number <= 200) {
+        console.log("Between 100 and 200");
+    } else if (number > 200) {
+        console.log("Greater than 200");
+    }
+
+}
+numberCheck(["95"]);
