05727e7fd1bf819da7cdf12817d7e41d2b5b8467
Petar Zhelev <majestic13@gmail.com>
2022-04-14 10:39:59 +0300

    exercise

---
+++ Basics/oscars.js
@@ -0,0 +1,28 @@
+function oscars(input) {
+
+
+        let actorName = input[0];
+        let academyPoints = Number(input[1]);
+        
+        arrLength = input.length;
+        for (let i = 3; i < arrLength; i += 2) {
+            
+            let juryName = input[i];
+            if ()
+
+            console.log(juryName.length);
+        }
+}
+oscars([
+  "Zahari Baharov",
+  "205",
+  "4",
+  "Johnny Depp",
+  "45",
+  "Will Smith",
+  "29",
+  "Jet Lee",
+  "10",
+  "Matthew Mcconaughey",
+  "39",
+]);
