a39f8cf3348967323ba4d27bd2abc47e8a9b108b
Petar Zhelev <majestic13@gmail.com>
2022-04-14 09:40:47 +0300

    exercise

---
+++ Basics/odd_or_even.js
@@ -0,0 +1,12 @@
+function oddoreven(input) {
+    let num = Number(input[0]);
+    
+  
+    if (num % 2 === 0) {
+      console.log("even");
+    } else {
+      console.log("odd");
+    }
+  }
+  oddoreven(["1"]);
+  
