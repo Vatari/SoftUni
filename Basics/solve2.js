ea076ae53e8e1a8203ae368d794b97deda305efc
Petar Zhelev <majestic13@gmail.com>
2022-04-17 11:07:33 +0300

    exercise

---
+++ Basics/solve2.js
@@ -0,0 +1,26 @@
+function BestPlayer(input) {
+  
+  let playerName = "";
+  let otherPlayer = "";
+  let goals = 0;
+
+
+  for (let i = 0; i < input.length; i++) {
+    let current = input[i];
+
+    if (i % 2 === 0) {
+      otherPlayer = current;
+    }
+    if (i % 2 === 1 && current > goals) {
+      goals = Number(current);
+      playerName = otherPlayer;
+    }
+  }
+  console.log(`${playerName} is the best player!`);
+  if (goals >= 3) {
+    console.log(`He has scored ${goals} goals and made a hat-trick !!!`);
+  } else {
+    console.log(`He has scored ${goals} goals.`);
+  }
+}
+BestPlayer(["Silva", "5", "Harry Kane", "10"]);
