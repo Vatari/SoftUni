ab14032e5c5505c99d7580b954cc1ebc3d95515e
unknown <majestic13@gmail.com>
2022-04-18 10:38:00 +0300

    exercise

---
+++ Basics/sumOfTwoNumbers.js
@@ -0,0 +1,7 @@
+function sumOfTwoNumbers(input) {
+  let firstNumber = Number(input[0]);
+  let secondNumber = Number(input[1]);
+  let sum = firstNumber + secondNumber;
+  console.log(sum);
+}
+sumOfTwoNumbers([`10`, `20`]);
