eadc90ea3f584b8b28b84e5e5350f76267b4f504
Petar Zhelev <majestic13@gmail.com>
2022-04-20 10:18:29 +0300

    exercise

---
+++ Basics/vowelsSum.js
@@ -0,0 +1,27 @@
+function vowelsSum(input) {
+  let text = input[0];
+  let result = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    let ch = text[i];
+    switch (ch) {
+      case "a":
+        result += 1;
+        break;
+      case "e":
+        result += 2;
+        break;
+      case "i":
+        result += 3;
+        break;
+      case "o":
+        result += 4;
+        break;
+      case "u":
+        result += 5;
+        break;
+    }
+  }
+  console.log(result);
+}
+vowelsSum(["bamboo"]);
