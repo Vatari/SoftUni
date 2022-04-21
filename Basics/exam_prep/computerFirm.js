8377da6a5a4075698210b426e40de7aa3dfa5a9b
Petar Zhelev <majestic13@gmail.com>
2022-04-21 09:36:55 +0300

    exercise

---
+++ Basics/exam_prep/computerFirm.js
@@ -0,0 +1,14 @@
+function computerFirm(input) {
+  let computerModels = Number(input[0]);
+
+  let index = 1;
+  let num = input[index] + "";
+
+  let n1 = Number(num[0]);
+  let n2 = Number(num[1]);
+  let n3 = Number(num[2]);
+
+  for (let a = num; a <= input.length; a++) {}
+  console.log(computerModels.toFixed(2));
+}
+computerFirm(["3", "103", "103", "103"]);
