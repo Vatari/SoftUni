d23c7a2ee266a6f0d364b4955f79b0734202298d
Petar Zhelev <majestic13@gmail.com>
2022-04-22 09:34:23 +0300

    exercise

---
+++ Fundamentals/addAndSubtractArray.js
@@ -0,0 +1,23 @@
+function addAndSubtract(arr) {
+  let newArr = [];
+  let sumOriginal = 0;
+  let sumNew = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    sumOriginal += arr[i];
+
+    if (arr[i] % 2 == 0) {
+      newArr.push((arr[i] += i));
+    } else {
+      newArr.push((arr[i] -= i));
+    }
+  }
+  for (let n of newArr) {
+    sumNew += n;
+  }
+  console.log(newArr);
+  console.log(sumOriginal);
+  console.log(sumNew);
+}
+addAndSubtract([5, 15, 23, 56, 35]);
+addAndSubtract([-5, 11, 3, 0, 2]);
