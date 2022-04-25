31423e903c1526ed41597b7356a2555948702b72
Petar Zhelev <majestic13@gmail.com>
2022-04-25 08:13:15 +0300

    exercise

---
+++ Fundamentals/calculator.js
@@ -0,0 +1,20 @@
+function calculator(num1, operator, num2) {
+  let sum = 0;
+  switch (operator) {
+    case "+":
+      sum = num1 + num2;
+      break;
+    case "-":
+      sum = num1 - num2;
+      break;
+    case "/":
+      sum = num1 / num2;
+      break;
+    case "*":
+      sum = num1 * num2;
+      break;
+  }
+  console.log(sum.toFixed(2));
+}
+calculator(5, "+", 10);
+calculator(25.5, "-", 3);
