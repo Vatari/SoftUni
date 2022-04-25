8828b03a784a6c1805a56cea814a7235f8320608
Petar Zhelev <majestic13@gmail.com>
2022-04-25 08:56:05 +0300

    exercise

---
+++ Fundamentals/centuriesToMinutes.js
@@ -0,0 +1,12 @@
+function centuriesToMinutes(century) {
+  let years = century * 100;
+  let yearDays = Math.trunc(years * 365.2422);
+  let hours = Math.trunc(yearDays * 24);
+  let minutes = Math.trunc(hours * 60);
+
+  console.log(
+    `${century} centuries = ${years} years = ${yearDays} days = ${hours} hours = ${minutes} minutes`
+  );
+}
+centuriesToMinutes(1);
+centuriesToMinutes(5);
