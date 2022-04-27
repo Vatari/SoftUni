2b43704359777bb042079f2d01679226458fbee5
Petar Zhelev <majestic13@gmail.com>
2022-04-27 09:16:44 +0300

    exercise

---
+++ Fundamentals/concatenateNames.js
@@ -0,0 +1,6 @@
+function concatenateNames(name1, name2, delimiter) {
+  console.log(`${name1}${delimiter}${name2}`);
+}
+concatenateNames("John", "Smith", "->");
+concatenateNames("Jan", "White", "<->");
+concatenateNames("Linda", "Terry", "=>");
