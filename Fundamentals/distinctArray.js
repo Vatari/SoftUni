23e2b2f6a3c331a294c591bb03859b228c32b7be
Petar Zhelev <majestic13@gmail.com>
2022-04-30 08:14:04 +0300

    exercise

--- Fundamentals/distinctArray.js
+++
@@ -1,9 +0,0 @@
-function distinctArray(arr) {
-  let newArr = [];
-
-  for (let el of arr) {
-    if (!newArr.includes(el)) newArr.push(el);
-  }
-  console.log(newArr.join(" "));
-}
-distinctArray([1, 2, 3, 4, 4]);
