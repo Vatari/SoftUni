445800908ab1b5efef09a966c2f9c3de6b639df4
Petar Zhelev <majestic13@gmail.com>
2022-05-02 08:36:20 +0300

    exercise

---
+++ Fundamentals/excellentGrade.js
@@ -0,0 +1,12 @@
+function grade(grade) {
+  if (grade >= 5.5) {
+    console.log("Excellent");
+  } else {
+    console.log("Not excellent");
+  }
+}
+grade(5.5);
+grade(5.2);
+grade(4.35);
+grade(3.1);
+grade(2.7);
