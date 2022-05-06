e745228c1b57ea88dfa2c2b8cd58d51b17d478bf
Petar Zhelev <majestic13@gmail.com>
2022-05-06 07:38:40 +0300

    exercise

---
+++ Fundamentals/lowerOrUpper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(str) {
+  let output = str == str.toUpperCase() ? "upper-case" : "lower-case";
+
+  /*   if (str == str.toUpperCase()) {
+    console.log("upper-case");
+  } else {
+    console.log("lower-case");
+  } */
+  console.log(output);
+}
+lowerOrUpper("L");
+lowerOrUpper("f");
