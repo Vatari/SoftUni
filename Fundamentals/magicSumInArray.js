cd22d7037c1b9ea7133407dee06bbd5642f2d57a
Petar Zhelev <majestic13@gmail.com>
2022-05-06 07:48:50 +0300

    exercise

---
+++ Fundamentals/magicSumInArray.js
@@ -0,0 +1,12 @@
+function magicSumInArray(arr, magicNum) {
+  for (let i = 0; i < arr.length; i++) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == magicNum) {
+        console.log(`${arr[i]} ${arr[j]}`);
+      }
+    }
+  }
+}
+magicSumInArray([1, 7, 6, 2, 19, 23], 8);
+magicSumInArray([14, 20, 60, 13, 7, 19, 8], 27);
+magicSumInArray([1, 2, 3, 4, 5, 6], 6);
