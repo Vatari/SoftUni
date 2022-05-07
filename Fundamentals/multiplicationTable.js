d23c9cdb3a73162bce30ce7fcedee7320c12b0ce
Petar Zhelev <majestic13@gmail.com>
2022-05-07 08:20:08 +0300

    exercise

---
+++ Fundamentals/multiplicationTable.js
@@ -0,0 +1,12 @@
+function multiplicationTable(num) {
+  let sum = 0;
+  for (i = 1; i <= 10; i++) {
+    let str = "";
+    for (j = 1; j <= i; j++) {
+      sum = num * i;
+    }
+
+    console.log(`${num} X ${i} = ${sum}`);
+  }
+}
+multiplicationTable(5);
