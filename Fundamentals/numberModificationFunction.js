335a014a311bc0e9441e5a2f8d76e95123d73ef7
Petar Zhelev <majestic13@gmail.com>
2022-05-08 08:21:02 +0300

    exercise

---
+++ Fundamentals/numberModificationFunction.js
@@ -0,0 +1,21 @@
+function numberModification(num) {
+  while (getAverage(num) < 5) {
+    num += "9";
+  }
+  console.log(num);
+
+  function sum(number) {
+    let numToString = number.toString();
+    let sum = 0;
+    for (let i = 0; i < numToString.length; i++) {
+      sum += Number(numToString[i]);
+    }
+    return sum;
+  }
+
+  function getAverage(num) {
+    return sum(num) / num.toString().length;
+  }
+}
+
+numberModification(101);
