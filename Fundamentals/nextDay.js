9f043c0ced632c0323b2e044df7d94450afa57a1
Petar Zhelev <majestic13@gmail.com>
2022-05-08 08:14:53 +0300

    exercise

---
+++ Fundamentals/nextDay.js
@@ -0,0 +1,14 @@
+/* let nextDay = new Date(year, month - 1, day + 1);
+let newYear = nextDay.getFullYear();
+let newMonth = nextDay.getMonth() + 1;
+let newDate = nextDay.getDate();
+console.log(`${newYear}-${newMonth}-${newDate}`); */
+
+function nextDay(year, month, day) {
+  let nextDay = new Date(year, month - 1, day + 1);
+  let newYear = nextDay.getFullYear();
+  let newMonth = nextDay.getMonth() + 1;
+  let newDate = nextDay.getDate();
+  console.log(`${newYear}-${newMonth}-${newDate}`);
+}
+nextDay(2016, 9, 30);
