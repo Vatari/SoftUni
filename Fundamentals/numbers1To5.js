5a619a1fe9854225661bf121ed8d153731a6e2c8
unknown <majestic13@gmail.com>
2022-05-09 10:54:42 +0300

    exercise

---
+++ Fundamentals/numbers1To5.js
@@ -0,0 +1,13 @@
+function numbers1To5() {
+  //FOR solution
+  //    for (let i = 1; i <= 5; i++) {
+  //    console.log(i);
+  //  }
+  //
+  let i = 1;
+  while (i <= 5) {
+    console.log(i);
+    i++;
+  }
+}
+numbers1To5();
