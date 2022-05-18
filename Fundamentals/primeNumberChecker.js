dd91bc45ddb9fcbdd4c746505f46dd89fc789f45
Petar Zhelev <majestic13@gmail.com>
2022-05-18 09:24:39 +0300

    exercise

---
+++ Fundamentals/primeNumberChecker.js
@@ -0,0 +1,15 @@
+function primeNumberChecker(num) {
+  let isPrime = true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      isPrime = false;
+    }
+  }
+
+  console.log(isPrime);
+}
+
+primeNumberChecker(7);
+primeNumberChecker(8);
+primeNumberChecker(81);
