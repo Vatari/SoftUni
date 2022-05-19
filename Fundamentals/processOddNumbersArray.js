23015ef5c76cf6265ad41ec16de03960ebb73e93
Petar Zhelev <majestic13@gmail.com>
2022-05-19 10:48:37 +0300

    exercise

---
+++ Fundamentals/processOddNumbersArray.js
@@ -0,0 +1,14 @@
+function processOddNumbers(arr) {
+  function oddElementsDoubled(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 2 != 0) {
+        arr[i] *= 2;
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr.reverse().join(" ");
+  }
+  console.log(oddElementsDoubled(arr));
+}
+processOddNumbers([10, 15, 20, 25]);
