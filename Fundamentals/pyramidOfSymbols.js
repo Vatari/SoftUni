4c825d6cda8f8aa17e4c765a1b644e7fb72c8a39
Petar Zhelev <majestic13@gmail.com>
2022-05-20 08:33:15 +0300

    exercise

---
+++ Fundamentals/pyramidOfSymbols.js
@@ -0,0 +1,10 @@
+// Display pyramid of number using JavaScript
+function pyramid(n) {
+    for (let i = 1; i <= n; i++) {
+       let str = ' '.repeat(n - i);
+       let str2 = '*'.repeat(i * 2 - 1);
+       console.log(str + str2 + str);
+    }
+ }
+ 
+ pyramid(50);
