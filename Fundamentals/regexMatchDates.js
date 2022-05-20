6afe3092bcd972b1d14694d03e23cdebae55a1f8
Petar Zhelev <majestic13@gmail.com>
2022-05-20 15:24:12 +0300

    exercise

---
+++ Fundamentals/regexMatchDates.js
@@ -0,0 +1,16 @@
+function matchDates(dates) {
+  let pattern =
+    /\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b/g;
+  let validDate = [];
+
+  while ((validDate = pattern.exec(dates)) !== null) {
+    let day = validDate.groups["day"];
+    let month = validDate.groups["month"];
+    let year = validDate.groups["year"];
+    console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+  }
+}
+
+matchDates(
+  "13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016"
+);
