0d7529344781fd94cbcc9bd3187eeb90dbc4c9cb
Petar Zhelev <majestic13@gmail.com>
2022-05-22 07:08:55 +0300

    exercise

---
+++ Fundamentals/reverseAnArrayOfNumbers.js
@@ -0,0 +1,17 @@
+function reverseAnArrayOfNumbers(n, arr) {
+  let newArr = [];
+  let output = " ";
+  for (let i = 0; i < n; i++) {
+    newArr.push(arr[i]);
+  }
+
+  for (let k = newArr.length - 1; k >= 0; k--) {
+    output += newArr[k];
+    output += " ";
+  }
+
+  console.log(output);
+}
+reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
+reverseAnArrayOfNumbers(4, [-1, 20, 99, 5]);
+reverseAnArrayOfNumbers(2, [66, 43, 75, 89, 47]);
