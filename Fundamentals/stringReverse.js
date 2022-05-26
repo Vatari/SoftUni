9d3e3a1574d978ebb5fba46c2d7df69ee4bcf9de
Petar Zhelev <majestic13@gmail.com>
2022-05-26 07:20:33 +0300

    exercise

---
+++ Fundamentals/stringReverse.js
@@ -0,0 +1,8 @@
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    console.log(newString);
+}
+reverseString('hello');
