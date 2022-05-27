96e3f5510b665e6c9c7ed3ed7cc6ad59bffc62fa
Petar Zhelev <majestic13@gmail.com>
2022-05-27 07:29:35 +0300

    exercise

---
+++ Fundamentals/sumFirstAndLastAray.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastAray(input) {
+  let result = input[0] + input[input.length - 1];
+
+  console.log(result);
+}
+sumFirstAndLastAray([20, 30, 40]);
+sumFirstAndLastAray([10, 17, 22, 33]);
+sumFirstAndLastAray([11, 58, 69]);
