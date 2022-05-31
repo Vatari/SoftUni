e946dd69d50614743b7371706e9d023c6e4b8d96
unknown <majestic13@gmail.com>
2022-05-31 07:41:55 +0300

    exercise

---
+++ Fundamentals/theatrePromotions.js
@@ -0,0 +1,43 @@
+function theatrePromotions(day, age) {
+  switch (day) {
+    case "Weekday":
+      if (age >= 0 && age <= 18) {
+        console.log("12$");
+      } else if (age > 18 && age <= 64) {
+        console.log("18$");
+      } else if (age > 64 && age <= 122) {
+        console.log("12$");
+      } else {
+        console.log("Error!");
+      }
+      break;
+
+    case "Weekend":
+      if (age >= 0 && age <= 18) {
+        console.log("15$");
+      } else if (age > 18 && age <= 64) {
+        console.log("20$");
+      } else if (age > 64 && age <= 122) {
+        console.log("15$");
+      } else {
+        console.log("Error!");
+      }
+      break;
+
+    case "Holiday":
+      if (age >= 0 && age <= 18) {
+        console.log("5$");
+      } else if (age > 18 && age <= 64) {
+        console.log("12$");
+      } else if (age > 64 && age <= 122) {
+        console.log("10$");
+      } else {
+        console.log("Error!");
+      }
+      break;
+  }
+}
+
+theatrePromotions("Weekday", 28);
+theatrePromotions("Holiday", -12);
+theatrePromotions("Holiday", 15);
