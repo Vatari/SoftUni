69f07f2076789dbdb30faf6848e034faf26d0188
Petar Zhelev <majestic13@gmail.com>
2022-06-01 11:40:46 +0300

    exercise

---
+++ Fundamentals/triplesOfLatinLetters.js
@@ -0,0 +1,14 @@
+function triplesOfLatinLetters(n) {
+  for (let i = 0; i < n; i++) {
+    let firstLetter = String.fromCharCode(i + 97);
+    for (let j = 0; j < n; j++) {
+      let secondLetter = String.fromCharCode(j + 97);
+      for (let k = 0; k < n; k++) {
+        let thirdLetter = String.fromCharCode(k + 97);
+        console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+      }
+    }
+  }
+}
+triplesOfLatinLetters("3");
+//triplesOfLatinLetters(3);
