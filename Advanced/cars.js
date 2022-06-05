cf0101537056ab1c6f357d8ea834761d4147b338
Petar Zhelev <majestic13@gmail.com>
2022-06-05 08:07:48 +0300

    exercise

---
+++ Advanced/cars.js
@@ -0,0 +1,31 @@
+function cars(arr) {
+  const data = {};
+
+  const instr = {
+    create: (n, inherits, n2) =>
+      (data[n] = inherits ? Object.create(data[n2]) : {}),
+    set: (n, k, v) => (data[n][k] = v),
+    print: (n) => {
+      const entry = [];
+      for (const key in data[n]) {
+        entry.push(`${key}:${data[n][key]}`);
+      }
+      console.log(entry.join(","));
+    },
+  };
+
+  arr.forEach((x) => {
+    const [c, n, k, v] = x.split(" ");
+
+    instr[c](n, k, v);
+  });
+}
+
+cars([
+  "create c1",
+  "create c2 inherit c1",
+  "set c1 color red",
+  "set c2 model new",
+  "print c1",
+  "print c2",
+]);
