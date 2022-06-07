984cfeefadeeda42cf7a5a72c7ff5b74fa16ec02
unknown <majestic13@gmail.com>
2022-06-07 08:24:22 +0300

    exercise

---
+++ Advanced/greatestCommonDivisor.js
@@ -0,0 +1,11 @@
+function solve(num1, num2) {
+  num1 = Math.abs(num1);
+  num2 = Math.abs(num2);
+  while (num2) {
+    let t = num2;
+    num2 = num1 % num2;
+    num1 = t;
+  }
+  console.log(num1);
+}
+solve(15, 5);
