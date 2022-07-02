b5e721b5c9af37ab14c14fdeba5c76672141f23f
Petar Zhelev <majestic13@gmail.com>
2022-07-02 08:13:56 +0300

    exercise

---
+++ Advanced/Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  let text = document.querySelector("#text");
+  text.style.display = "inline";
+  let mainString = document.querySelector("#more");
+
+  mainString.style.display = "none";
+}
