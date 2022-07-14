3ae4b4d294fbbd4e5239e3bd46e4f2236532d601
Petar Zhelev <majestic13@gmail.com>
2022-07-14 08:35:31 +0300

    exercise

---
+++ Advanced/Notification/app.js
@@ -0,0 +1,6 @@
+function notify (message) {
+  let notification = document.getElementById(`notification`)
+  notification.textContent = message
+  notification.style.display = 'block'
+  notification.addEventListener('click', () => notification.style.display = 'none')
+}
