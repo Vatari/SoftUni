33b472b1f97f6e21f1a3207735dc0881d2db45df
Petar Zhelev <majestic13@gmail.com>
2022-08-07 08:17:07 +0300

    exercise

---
+++ JS Applications/SoftTerest/src/util.js
@@ -0,0 +1,27 @@
+export function getuserData() {
+  return JSON.parse(localStorage.getItem("userData"));
+}
+
+export function getAccessToken() {
+  const user = getUserData();
+  if (user) {
+    return user.accessToken;
+  } else {
+    return null;
+  }
+}
+
+export function clearUserData() {
+  localStorage.removeItem("userData");
+}
+
+export function setUserData(data) {
+  localStorage.setItem("userData", JSON.stringify(data));
+}
+export function createSubmitHandler(ctx, handler) {
+  return function (ev) {
+    ev.preventDefault();
+    const formData = Object.fromEntries(new FormData(ev.target));
+    handler(ctx, formData, ev);
+  };
+}
