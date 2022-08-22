baf3714d4438dae8a3b1e47247199e8f157fe7f1
unknown <majestic13@gmail.com>
2022-08-22 11:35:55 +0300

    exercise

---
+++ JS Applications/exams/exam_prep1/Meme Lounge/src/api/requester.js
@@ -0,0 +1,48 @@
+import { clearUserData } from "../util.js";
+import { notify } from "../views/notify.js";
+
+const host = "http://localhost:3030";
+
+async function request(method, url, data) {
+  const options = {
+    method,
+    headers: {},
+  };
+
+  if (data !== undefined) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(data);
+  }
+
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  if (userData != null) {
+    options.headers["X-Authorization"] = userData.accessToken;
+  }
+
+  try {
+    const res = await fetch(host + url, options);
+
+    if (res.ok == false) {
+      if (res.status == 403) {
+        clearUserData();
+      }
+      const error = await res.json();
+      throw Error(error.message);
+    }
+
+    if (res.status == 204) {
+      return res;
+    } else {
+      return await res.json();
+    }
+  } catch (err) {
+    notify(err.message);
+    throw err;
+  }
+}
+
+export const get = request.bind(null, "get");
+export const post = request.bind(null, "post");
+export const put = request.bind(null, "put");
+export const del = request.bind(null, "delete");
